a612cf03259b9f4084a1b8c5c9edbbd0d8fddd50
ErickMaia <erickmaiatmariano@gmail.com>
2022-03-30 05:00:02 -0300

    Added new class PQueue. I'm going to write its properties and methods later.

---
+++ src/DataStructures/Types/PQueue.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructures.Types
+{
+    public class PQueue<T> where T : IComparable<T>
+    {
+        
+    }
+}
